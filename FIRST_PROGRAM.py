2a51ec68cbd220e9b0edd19d85b55e171eea9e01
Mira <55579671+MiraSuwala@users.noreply.github.com>
2020-02-15 20:44:49 +0100

    refactoring
    zmiana wielkości liter w zmiennych

--- FIRST_PROGRAM.py
+++ FIRST_PROGRAM.py
@@ -1,24 +1,24 @@
  #funkcja input(), str(), int()
 
 print('Hi, \nwhat is your nickname?') #Pyta o imie.
-Name = input()
+name = input()
 
 
-Name = Name.title()
-if Name: #pusty string ma wartość falsey.
-    print('Nice to meet you, ', Name)
+name = name.title()
+if name: #pusty string ma wartość falsey.
+    print('Nice to meet you, ', name)
 else:
     print('Noname is no name :(')
 
 
 
 while True:
-    print(Name + ', how old are you?') #pyta o wiek
+    print(name + ', how old are you?') #pyta o wiek
     Wiek = input() #podajesz wiek
 
     try: 
-        int(Wiek)
-        if int(Wiek) <= 99 and int(Wiek) > 0: #sprawdza czy jest to liczba od 0 do 99 i przerywa pętle
+        int(wiek)
+        if int(wiek) <= 99 and int(wiek) > 0: #sprawdza czy jest to liczba od 0 do 99 i przerywa pętle
             break
         else:
             print('Are you sure?') #podaje komunikat jeśli jest poza przedziałem
@@ -26,7 +26,7 @@ while True:
         print('it is not a number.') 
 
 
-print('You born in ' + str(2020 - int(Wiek))) #podaje datę urodzenia
+print('You born in ' + str(2020 - int(wiek))) #podaje datę urodzenia
 
-
+#Radziu pamietaj o prawidlowym nazywaniu zmiennych malymi_literami_z_daszami
 
